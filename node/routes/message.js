f1e5aecf8bea6c76630a63851604ce6770d6ed1e
nadongjun <aa9814526@gmail.com>
2021-03-14 18:25:36 -0700

    Edit message.js

--- node/routes/message.js
+++ node/routes/message.js
@@ -13,13 +13,23 @@ var router = express.Router();
 // 가져오기, 로그인한 사용자만 
 router.post('/message', isLoggedIn, async (req, res, next) => {
 
+  const messageList = await msgTB.create({id: req.body.user_id, msg: req.body.msg})
+  .then(result => {
+     res.json(result);
+  })
+  .catch(err => {
+     console.error(err);
+  });
+
         return res.status(200).json("메세지 저장됐어요");
    
   });
 // 자기 채팅 목록을 가져옴
   router.get('/message', isLoggedIn, async (req, res, next) => { 
-
-    return res.status(200).json("저장된 메세지 저장됐어요"); 
+    const messageList = await msgTB.findOne({
+      where: { id: user.id }
+    })
+    return res.status(200).json("저장된 메세지 보냄"); 
  });
   
 
