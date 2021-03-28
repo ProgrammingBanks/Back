9cbcae8be62fd640a03e0d9b7a8be6a17180cc0c
KUD <kimud6003@gmail.com>
2021-03-28 12:45:40 +0900

    edit passport

--- node/passport/index.js
+++ node/passport/index.js
@@ -4,13 +4,22 @@ const { clientTB } = require('../models');
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
-    done(null, user.clientEmail);
+    done(null, { 
+      csn: user.csn,
+      nsc: user.nsc
+    });
   });
 
   passport.deserializeUser(async (id, done) => {
     try {
-      const user = await clientTB.findOne({ where: {clientEmail }});
-      done(null, user); // req.user
+      /*사용자 요청마다 수행 : csn 비교하여 요청 유효 체크 */
+      const user = await clientTB.findOne({ 
+        attributes: ["csn", "nsc", "clientName", "clientEmail"],
+        where: {
+          csn: id.csn,
+        }});
+      /*req.user에 로그인 정보 저장 */
+      done(null, user);
     } catch (error) {
       console.error(error);
       done(error);
--- node/passport/local.js
+++ node/passport/local.js
@@ -1,28 +1,59 @@
 const passport = require('passport');
 const { Strategy: LocalStrategy } = require('passport-local');
 const bcrypt = require('bcrypt');
-const { clientTB } = require('../models');
+const { clientTB, sequelize } = require('../models');
+const h = require('../lib/header');
 
 module.exports = () => {
   passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
+    usernameField: 'clientEmail',
+    passwordField: 'clientPw'
   }, async (email, password, done) => {
+    const loginTrn = await sequelize.transaction();
     try {
       const user = await clientTB.findOne({
-        where: { clientEmail }
-      });
+          attributes: ['csn', 'nsc', 'clientPw', 'clientName'],
+          where: { 
+            clientEmail: email 
+          }});
+      /* 입력한 이메일 사용자 정보 없음 */
       if (!user) {
-        return done(null, false, { reason: '존재하지 않는 이메일입니다!' });
+        return done(null, false, { 
+          resCode: h.resCode.cltAcc02.noUserData,
+          msgType: h.msgType.cltAcc02Res,
+          reason: '존재하지 않는 이메일' 
+        });
       }
       const result = await bcrypt.compare(password, user.clientPw);
       if (result) {
-        return done(null, user);
+        /*로그인 횟수 로그인마다 +1*/
+        user.nsc ++ ; 
+        await clientTB.update({
+          nsc: user.nsc
+        }, {
+            where: {
+              csn: user.csn
+            }
+        }, {
+            transaction: loginTrn
+        });
+        await loginTrn.commit();
+        return done(null, {
+          csn: user.csn,
+          nsc: user.nsc,
+          clientName: user.clientName});
       }
-      return done(null, false, { reason: '비밀번호가 틀렸습니다.' });
+      return done(null, false, {
+        resCode: h.resCode.cltAcc02.wrongPw, 
+        msgType: h.msgType.cltAcc02Res,
+        reason: '비밀번호가 틀렸습니다'});
     } catch (error) {
+      await loginTrn.rollback();
       console.error(error);
-      return done(error);
+      return done({
+        resCode: h.resCode.cltAcc02.unknownErr, 
+        msgType: h.msgType.cltAcc02Res,
+        reason: '알수없는 오류'});
     }
   }));
 };
